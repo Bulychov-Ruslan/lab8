af196ce5c27a1bcd89f2e92275d4476caa1332c4
Bulychov-Ruslan <rusbul02@gmail.com>
2023-03-25 16:31:32 +0600

    master commit 2

--- main.py
+++ main.py
@@ -17,3 +17,19 @@ new_country, new_river = input().split()
 rivers.update({new_country: new_river})
 
 print(rivers)
+
+
+
+# # -------------------------------------3------------------------------------------
+# n = int(input())
+# result = {}
+
+# for i in range(n):
+#     number, name = input().split()
+#     result[name] = number
+
+# print(result)
+
+# name = input()
+# if name in result:
+#     print(result[name])
